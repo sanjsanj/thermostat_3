8574044ce38e7850c3f6a350c76a80a3514185a5
sanjsanj <sanjsanj@hotmail.com>
2015-07-18 09:32:21 +0100

    setup karma

---
+++ test/karma.conf.js
@@ -0,0 +1,23 @@
+module.exports = function(config) {
+  config.set({
+    basePath: '../',
+    frameworks: ['jasmine'],
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-route/angular-route.js',
+      'src/*.js',
+      'test/*.js'
+    ],
+    exclude: [
+    ],
+    preprocessors: {
+    },
+    reporters: ['progress'],
+    port: 9876,
+    colors: true,
+    logLevel: config.LOG_INFO,
+    autoWatch: true,
+    browsers: ['PhantomJS'],
+    singleRun: false
+  })
+}
