8c513c13ea2b7946ca01e83aa48272596152a8a2
sanjsanj <sanjsanj@hotmail.com>
2015-07-24 20:38:16 +0100

    travis-CI - switch karma browser to chrome

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -12,12 +12,21 @@ module.exports = function(config) {
     ],
     preprocessors: {
     },
-    reporters: ['progress'],
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: ['PhantomJS'],
-    singleRun: true
+    browsers: ['Chrome'],
+    customLaunchers: {
+  Chrome_travis_ci: {
+    base: 'Chrome',
+    flags: ['--no-sandbox']
+    }
+  },
+  singleRun: false
+  };
+  if(process.env.TRAVIS){
+  configuration.browsers = ['Chrome_travis_ci'];
   }
-}
+  config.set(configuration);
+};
